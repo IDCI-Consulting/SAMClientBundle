acfa0273709c2a65d3159af6c2d69d77c423a2c2
Maëva GRONDIN <maeva.grondin@idci-consulting.fr>
2025-01-07 11:42:34 +0100

    Add BusinessDealPayload and BusinessDealProgress models

---
+++ src/Model/BusinessDealPayload.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace IDCI\Bundle\SAMClientBundle\Model;
+
+class BusinessDealPayload
+{
+    private int $id;
+    private string $internalNumber;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getInternalNumber(): string
+    {
+        return $this->internalNumber;
+    }
+
+    public function setInternalNumber(string $internalNumber): self
+    {
+        $this->internalNumber = $internalNumber;
+
+        return $this;
+    }
+}
