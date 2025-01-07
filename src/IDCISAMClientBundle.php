8ce972d6d892e1c3e28142ba760510d7d475a992
Maëva GRONDIN <maeva.grondin@idci-consulting.fr>
2025-01-07 11:41:23 +0100

    Configure guzzle http client

--- src/IDCISAMClientBundle.php
+++ src/IDCISAMClientBundle.php
@@ -2,7 +2,9 @@
 
 namespace IDCI\Bundle\SAMClientBundle;
 
+use IDCI\Bundle\SAMClientBundle\DependencyInjection\Compiler\EightPointsGuzzleClientInjectionCompilerPass;
 use IDCI\Bundle\SAMClientBundle\DependencyInjection\IDCISAMClientExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
@@ -11,5 +13,14 @@ class IDCISAMClientBundle extends AbstractBundle
     public function getContainerExtension(): ?ExtensionInterface
     {
         return new IDCISAMClientExtension();
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container
+            ->addCompilerPass(new EightPointsGuzzleClientInjectionCompilerPass())
+        ;
     }
 }
